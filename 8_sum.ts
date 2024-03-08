215792dd36c49ba9e19ceda9616e00cabcaade24
laibairfankhan <162556101+laibairfankhan@users.noreply.github.com>
2024-03-07 22:10:26 -0800

    Add files via upload

---
+++ 8_sum.ts
@@ -0,0 +1,29 @@
+//You should create four lines that look like this:
+
+//console.log(5 + 3)
+
+//Your output should simply be four lines with the number 8 appearing once on each line.
+
+// Print the result of the addition operation: 2 + 6 = 8.
+
+console.log(2+6);
+
+//Result in 8.
+
+// Print the result of the subtraction operation: 12 - 4 = 8.
+
+console.log(12-4);
+
+//Result is 8.
+
+// Print the result of the multiplication operation: 2 * 4 = 8.
+
+console.log(2*4);
+
+//Result in 8.
+
+// Print the result of the division operation: 16 / 2 = 8.
+
+console.log(16/2);
+
+//Result in 8.
