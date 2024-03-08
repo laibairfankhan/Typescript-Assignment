3e68437e05a0917994a6fefd77651dfeb60f9f03
laibairfankhan <162556101+laibairfankhan@users.noreply.github.com>
2024-03-07 22:08:34 -0800

    Add files via upload

---
+++ 7 Number_Eight.js
@@ -0,0 +1,10 @@
+"use strict";
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+const Addition = (2 + 6);
+console.log(Addition);
+const Subtraction = (12 - 4);
+console.log(Subtraction);
+const Multiplication = (2 * 4);
+console.log(Multiplication);
+const Division = (16 / 2);
+console.log(Division);
---
+++ 7 Number_Eight.ts
@@ -0,0 +1,38 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+
+// Perform addition to get the result of 8.
+
+const Addition: Number = (2+6);
+
+// In this operation, 2 + 6 equals 8. The result is stored in the 'Addition' variable and printed.
+
+console.log(Addition);
+
+
+// Perform subtraction to get the result of 8.
+
+
+const Subtraction: Number = (12-4);
+
+// Subtracting 4 from 12 results in 8. The 'Subtraction' variable store  the result, which is then printed.
+
+console.log(Subtraction);
+
+
+// Perform multiplication to get the result of 8.
+
+const Multiplication: Number = (2*4);
+
+// Comment: Multiplying 2 by 4 result 8. The 'Multiplication' variable stores the result, and it is printed.
+
+console.log(Multiplication);
+
+// Perform division to get the result of 8.
+
+const Division: Number = (16/2);
+
+// Dividing 16 by 2 gives 8. The 'Division' variable stores the quotient, which is then printed.
+
+
+console.log(Division);
