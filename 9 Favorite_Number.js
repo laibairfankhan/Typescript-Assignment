cc0021f68601574a184e93a4d5df33128026f3b3
laibairfankhan <162556101+laibairfankhan@users.noreply.github.com>
2024-03-08 11:25:08 -0800

    Delete 9 Favorite_Number.js

--- 9 Favorite_Number.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-Object.defineProperty(exports, "__esModule", { value: true });
-const Favorite_number = 7;
-const message = (`My favorite number is ${Favorite_number}`);
-console.log(message);
