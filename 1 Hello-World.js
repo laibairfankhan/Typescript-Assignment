c58eb9a9b768d199febcdf3b35dc1e2aa8729a23
laibairfankhan <162556101+laibairfankhan@users.noreply.github.com>
2024-03-07 21:57:19 -0800

    Add files via upload

---
+++ 1 Hello-World.js
@@ -0,0 +1,9 @@
+"use strict";
+//Install Node.js, TypeScript and VS Code on your computer.
+//I Have Done My  Node.js ,TypesScript and Visual Studio Code in My Laptop.
+//This is MY First Program:
+// We declare a constant variable named "Message" of type String.
+// The value assigned to Message is "Hello World" .
+const Message = `Hello World`;
+// Now, we print the Message to the console.
+console.log(Message);
