59065d709ad9ce29698ff723607b7372d56250b7
laibairfankhan <162556101+laibairfankhan@users.noreply.github.com>
2024-03-07 21:23:42 -0800

    Add files via upload

---
+++ 2 Personal_Message.js
@@ -0,0 +1,6 @@
+"use strict";
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+//Creat a Variable:
+const Person_Name = `Eric`;
+//print the message:
+console.log(`\u201cHello ${Person_Name}, would you like to learn some python today?\u201d`);
