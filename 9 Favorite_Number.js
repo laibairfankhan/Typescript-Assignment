a498acd7d757545d804b27affc9f427b126cb185
laibairfankhan <162556101+laibairfankhan@users.noreply.github.com>
2024-03-07 22:11:47 -0800

    Add files via upload

---
+++ 9 Favorite_Number.js
@@ -0,0 +1,6 @@
+"use strict";
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+Object.defineProperty(exports, "__esModule", { value: true });
+const Favorite_number = 7;
+const message = (`My favorite number is ${Favorite_number}`);
+console.log(message);
---
+++ 9 Favorite_Number.ts
@@ -0,0 +1,16 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+// Store your favorite number in a variable.
+
+const Favorite_number: Number = 7;
+
+
+// Create a message using template literals to reveal your favorite number.
+
+const message: string = (`My favorite number is ${Favorite_number}`);
+
+
+// Print the message to the console
+
+console.log(message);
