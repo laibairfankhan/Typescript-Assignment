9650f008efb2a62ecc650b681dd128eea0be8a18
laibairfankhan <162556101+laibairfankhan@users.noreply.github.com>
2024-03-07 22:14:13 -0800

    Add files via upload

---
+++ 11 Names.js
@@ -0,0 +1,8 @@
+"use strict";
+//[Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+// Make a aaray for friends name
+const Friends_Name = [` Arzoo`, ` Liza`, ` Naziya`, ` Esha`];
+for (let i = 0; i < Friends_Name.length; i++) {
+    //Print friends name
+    console.log(Friends_Name[i]);
+}
---
+++ 11 Names.ts
@@ -0,0 +1,21 @@
+//[Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+
+// Create an array to store friends' names.
+
+const Friends_Name: string[] = [` Arzoo`,` Liza`,` Naziya`,` Esha`];
+
+
+// Iterate through the array to print each friend's name.
+
+for(let i=0; i<Friends_Name.length; i++){
+
+
+   // Print each friend's name one at a time.
+
+    console.log(Friends_Name[i]);
+}
+
+
+
+
