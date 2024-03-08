b199ef055e0b017152842ae46ad1e0127d2c11a2
laibairfankhan <162556101+laibairfankhan@users.noreply.github.com>
2024-03-08 01:52:17 -0800

    Add files via upload

---
+++ 20 Rivers_Name.js
@@ -0,0 +1,5 @@
+"use strict";
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+const Rivers_name = [`Jhelum`, `Soan `, `Indus`, `Beas`];
+console.log(`The list of RiversName.`);
+Rivers_name.forEach(Rivers_name => console.log(Rivers_name));
---
+++ 20 Rivers_Name.ts
@@ -0,0 +1,10 @@
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+// Create an array to store the names of rivers.
+const Rivers_name: string[] = [`Jhelum`  ,`Soan `, `Indus`, `Beas`];
+
+//Print a message to introduce the list.
+console.log(`The list of RiversName.`);
+
+// Use forEach to go through each river in the list and print its name.
+Rivers_name.forEach(Rivers_name => console.log(Rivers_name));
