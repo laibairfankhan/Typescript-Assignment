661616f9cc11a3a9e669f090c13bcbe75cf886cb
laibairfankhan <162556101+laibairfankhan@users.noreply.github.com>
2024-03-07 22:13:07 -0800

    Add files via upload

---
+++ 10 Adding_Comment.js
@@ -0,0 +1,8 @@
+"use strict";
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does
+//Author : Laiba
+//Date; 26/02/2024
+const a = 6; //declare a varaible [a] with number.
+const b = 8; //declare a varaible [b] with number.
+const sum = a + b; //calculate the sum of two number.
+console.log(sum); //Display the sum
---
+++ 10 Adding_Comment.ts
@@ -0,0 +1,21 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does
+
+//Author : Laiba
+//Date; 26/02/2024
+
+
+// Declare a variable 'a' and assign it the value 6.
+
+const a = 6; 
+
+// Declare a variable 'b' and assign it the value 8.
+
+const b = 8; 
+
+// Calculate the sum of variables 'a' and 'b' and store it in the variable 'sum'.
+
+const sum: number = a + b; 
+
+// Display the sum in the console.
+
+console.log(sum) 
