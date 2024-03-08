201c69bbbdde5a49b0b11dac5aee8ca9aca15aae
laibairfankhan <162556101+laibairfankhan@users.noreply.github.com>
2024-03-08 02:00:19 -0800

    Add files via upload

---
+++ 21 creat_object.js
@@ -0,0 +1,9 @@
+"use strict";
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+const languageCoure = {
+    courseName: `English language`,
+    location: `North Karachi`,
+    Institute: `Sir Sham's Anglophile English Language Centre`,
+    StudentInOneClass: `80`,
+};
+console.log(languageCoure);
