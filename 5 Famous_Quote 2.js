aec2ddf0824412b60bf783c2af0c2846ca2c156d
laibairfankhan <162556101+laibairfankhan@users.noreply.github.com>
2024-03-07 22:07:20 -0800

    Add files via upload

---
+++ 5 Famous_Quote 2.js
@@ -0,0 +1,5 @@
+"use strict";
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+const Famous_person = "Quaid-e-Azam";
+const message = (`${Famous_person} said, \u201C do not believe in taking the right decision . I take a decision and make it right.\u201D`);
+console.log(message);
